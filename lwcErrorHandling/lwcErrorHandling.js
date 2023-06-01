60ae7a963b719683af748fd5d452e4f3d2955f1c
Manoj Goswami <108041815+manojpgoswamigit@users.noreply.github.com>
2023-06-01 12:57:53 -0300

    Add files via upload

---
+++ lwcErrorHandling/lwcErrorHandling.js
@@ -0,0 +1,95 @@
+/*
+write these in parent component 
+
+     <template if:true={error}>
+        <c-lwc-error-handling error={error} onerrorclosed={handleError}></c-lwc-error-handling>
+     </template>
+
+// handle error from child components
+    errorCallback(error, stack){
+        this.error = error;
+    }
+    
+    // handle errors from self (submit button press)
+    handleFormError(event){
+        this.error = event.detail;
+    }
+    
+    // handle errors from self
+    handleError(error){
+        this.error =  error;   
+    }
+
+*/
+
+import { LightningElement, api, track, wire } from "lwc";
+export default class LwcErrorHandling extends LightningElement {
+    @api error;
+    errorMessageToDisplay = '';
+
+    // this runs every time this component is loaded
+    connectedCallback() {
+        this.errorMessageToDisplay = 'An error occurred : \n';
+        console.log(this.error);
+
+        // getting meaningful error message depending on error type
+        // handles errors from wire & regular apex methods
+        if (this.error.body) {
+            if (Array.isArray(this.error.body)) {
+                this.errorMessageToDisplay += this.error.body.map(e => e.message).join(', ');
+            }
+            else if (typeof this.error.body === 'object') {
+                let fieldErrors = this.error.body.fieldErrors;
+                let pageErrors = this.error.body.pageErrors;
+                let duplicateResults = this.error.body.duplicateResults;
+                let exceptionError = this.error.body.message;
+
+                if (exceptionError && typeof exceptionError === 'string') {
+                    this.errorMessageToDisplay += exceptionError;
+                }
+
+                if (fieldErrors) {
+                    for (var fieldName in fieldErrors) {
+                        let errorList = fieldErrors[fieldName];
+                        for (var i = 0; i < errorList.length; i++) {
+                            this.errorMessageToDisplay += errorList[i].statusCode + ' ' + fieldName + ' ' + errorList[i].message + ' ';
+                        }
+                    }
+                }
+
+                if (pageErrors && pageErrors.length > 0) {
+                    for (let i = 0; i < pageErrors.length; i++) {
+                        this.errorMessageToDisplay += pageErrors[i].statusCode + ' ' + pageErrors[i].message;
+                    }
+                }
+
+                if (duplicateResults && duplicateResults.length > 0) {
+                    this.errorMessageToDisplay += 'duplicate result error';
+                }
+            }
+        }
+        // handles errors from the lightning record edit form
+        if (this.error.message) {
+            this.errorMessageToDisplay += this.error.message;
+        }
+        if (this.error.detail) {
+            this.errorMessageToDisplay += this.error.detail;
+        }
+
+    }
+
+    // runs when the close button on the error banner is clicked
+    closeError() {
+        // reset variables
+        this.errorMessageToDisplay = '';
+        this.error = null;
+        // create a custom event for resetting the error state
+        const closeErrorEvent = new CustomEvent("errorclosed", {
+            detail: this.error
+        });
+
+        // dispatch the custom event to the parent component, this resets the error and hides this component
+        // if the parent component triggers another error, this entire component loads again and runs through the connectedCallback logic
+        this.dispatchEvent(closeErrorEvent);
+    }
+}
